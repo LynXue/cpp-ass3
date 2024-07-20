4941c72b2eb00501d441d2a366556316c2870cf5
LynXue <xuecuiling9909@gmail.com>
2024-07-20 18:53:23 +1000

    Add weighted_edge and unweighted_edge classes

--- src/gdwg_graph.h
+++ src/gdwg_graph.h
@@ -1,9 +1,14 @@
 #ifndef GDWG_GRAPH_H
 #define GDWG_GRAPH_H
 
+#include <boost/functional/hash.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
 #include <optional>
 #include <string>
 #include <utility>
+#include <vector>
 
 // TODO: Make both graph and edge generic
 //       ... this won't just compile
@@ -24,6 +29,42 @@ namespace gdwg {
 	};
 
 	template<typename N, typename E>
+	class weighted_edge : public edge<N, E> {
+	 public:
+		weighted_edge(N src, N dst, E weight)
+		: src_(std::make_shared<N>(src))
+		, dst_(std::make_shared<N>(dst))
+		, weight_(weight) {}
+
+		auto print_edge() const -> std::string override;
+		auto is_weighted() const -> bool override;
+		auto get_weight() const -> std::optional<E> override;
+		auto get_nodes() const -> std::pair<N, N> override;
+
+	 private:
+		std::shared_ptr<N> src_;
+		std::shared_ptr<N> dst_;
+		E weight_;
+	};
+
+	template<typename N, typename E>
+	class unweighted_edge : public edge<N, E> {
+	 public:
+		unweighted_edge(N src, N dst)
+		: src_(std::make_shared<N>(src))
+		, dst_(std::make_shared<N>(dst)) {}
+
+		auto print_edge() const -> std::string override;
+		auto is_weighted() const -> bool override;
+		auto get_weight() const -> std::optional<int> override;
+		auto get_nodes() const -> std::pair<N, N> override;
+
+	 private:
+		std::shared_ptr<N> src_;
+		std::shared_ptr<N> dst_;
+	};
+
+	template<typename N, typename E>
 	class graph {
 	 public:
 		using edge = gdwg::edge<N, E>;
@@ -40,6 +81,8 @@ namespace gdwg {
 		auto operator=(graph const& other) -> graph&;
 
 	 private:
+		std::unordered_set<std::shared_ptr<N>> nodes_;
+		std::unordered_map<std::pair<N, N>, std::vector<std::shared_ptr<edge>>, boost::hash<std::pair<N, N>>> edges_;
 	};
 } // namespace gdwg
 

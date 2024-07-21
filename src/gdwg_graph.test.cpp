1c4e53e0cc408e4f9c0fb8cd715af0c3bfa5ccf6
LynXue <xuecuiling9909@gmail.com>
2024-07-21 23:10:32 +1000

    Add basic insert_node and insert_edge tests

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -3,12 +3,42 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("basic test") {
-	// These are commented out right now
-	//  because withour your implementation
-	//  it will not compile. Uncomment them
-	//  once you've done the work
-	/*auto g = gdwg::graph<int, std::string>{};
+	auto g = gdwg::graph<int, std::string>{};
 	auto n = 5;
 	g.insert_node(n);
-	CHECK(g.is_node(n));*/
+	REQUIRE(g.is_node(n) == true);
+}
+
+TEST_CASE("Insert nodes") {
+	auto g = gdwg::graph<std::string, int>{};
+	REQUIRE(g.insert_node("A") == true);
+	REQUIRE(g.insert_node("B") == true);
+	REQUIRE(g.insert_node("A") == false);
+}
+
+TEST_CASE("Insert edges") {
+	auto g = gdwg::graph<std::string, int>{};
+	g.insert_node("A");
+	g.insert_node("B");
+
+	REQUIRE(g.insert_edge("A", "B", 5) == true);
+	REQUIRE(g.insert_edge("A", "B", 10) == true);
+	REQUIRE(g.insert_edge("A", "B", 5) == false);
+}
+
+TEST_CASE("Insert edge with non-existent nodes throws runtime_error") {
+	auto g = gdwg::graph<std::string, int>{};
+	g.insert_node("A");
+	g.insert_node("B");
+
+	REQUIRE_THROWS_WITH(g.insert_edge("X", "B", 5),
+	                    "Cannot call gdwg::graph<N, E>::insert_edge when either src or dst node does not exist");
+
+	REQUIRE_THROWS_WITH(g.insert_edge("A", "Y", 10),
+	                    "Cannot call gdwg::graph<N, E>::insert_edge when either src or dst node does not exist");
+
+	REQUIRE_THROWS_WITH(g.insert_edge("X", "Y", 15),
+	                    "Cannot call gdwg::graph<N, E>::insert_edge when either src or dst node does not exist");
+
+	REQUIRE_NOTHROW(g.insert_edge("A", "B", 5));
 }

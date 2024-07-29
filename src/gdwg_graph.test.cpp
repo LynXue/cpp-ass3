ce17e9d2a3914daf7d5c6c4cc903629cf76af940
LynXue <xuecuiling9909@gmail.com>
2024-07-29 20:07:05 +1000

    Add erase edge using iterator tests

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -17,9 +17,7 @@ TEST_CASE("Insert nodes") {
 }
 
 TEST_CASE("Insert edges") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
 
 	REQUIRE(g.insert_edge("A", "B", 5) == true);
 	REQUIRE(g.insert_edge("A", "B", 10) == true);
@@ -27,9 +25,7 @@ TEST_CASE("Insert edges") {
 }
 
 TEST_CASE("Insert edge with non-existent nodes throws runtime_error") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
 
 	REQUIRE_THROWS_WITH(g.insert_edge("X", "B", 5),
 	                    "Cannot call gdwg::graph<N, E>::insert_edge when either src or dst node does not exist");
@@ -44,19 +40,13 @@ TEST_CASE("Insert edge with non-existent nodes throws runtime_error") {
 }
 
 TEST_CASE("replace non-existing node") {
-	auto g = gdwg::graph<int, std::string>{};
-	g.insert_node(1);
-	g.insert_node(2);
-	g.insert_node(3);
+	auto g = gdwg::graph<int, std::string>{1, 2, 3};
 
 	REQUIRE_THROWS_WITH(g.replace_node(4, 5), "Cannot call gdwg::graph<N, E>::replace_node on a node that doesn't exist");
 }
 
 TEST_CASE("replace existing node and update edges") {
-	auto g = gdwg::graph<int, std::string>{};
-	g.insert_node(1);
-	g.insert_node(2);
-	g.insert_node(3);
+	auto g = gdwg::graph<int, std::string>{1, 2, 3};
 	g.insert_edge(1, 2, "edge1");
 	g.insert_edge(2, 3, "edge2");
 	g.insert_edge(1, 3, "edge3");
@@ -69,18 +59,13 @@ TEST_CASE("replace existing node and update edges") {
 }
 
 TEST_CASE("replace node with existing node") {
-	auto g = gdwg::graph<int, std::string>{};
-	g.insert_node(1);
-	g.insert_node(2);
-	g.insert_node(3);
+	auto g = gdwg::graph<int, std::string>{1, 2, 3};
 
 	REQUIRE(g.replace_node(1, 2) == false);
 }
 
 TEST_CASE("merge_replace_node: old_data or new_data does not exist") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
 
 	g.insert_edge("A", "B", 1);
 
@@ -94,9 +79,8 @@ TEST_CASE("merge_replace_node: old_data or new_data does not exist") {
 }
 
 TEST_CASE("Erase a weighted edge that exists") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
+
 	g.insert_edge("A", "B", 1);
 
 	REQUIRE(g.erase_edge("A", "B", 1) == true);
@@ -104,9 +88,7 @@ TEST_CASE("Erase a weighted edge that exists") {
 }
 
 TEST_CASE("Erase an unweighted edge that exists") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
 	g.insert_edge("A", "B");
 
 	REQUIRE(g.erase_edge("A", "B") == true);
@@ -114,9 +96,7 @@ TEST_CASE("Erase an unweighted edge that exists") {
 }
 
 TEST_CASE("Erase an unweighted edge that does not exist") {
-	auto g = gdwg::graph<std::string, int>{};
-	g.insert_node("A");
-	g.insert_node("B");
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
 
 	REQUIRE(g.erase_edge("A", "B") == false);
 }
@@ -129,4 +109,34 @@ TEST_CASE("Erase an edge when either src or dst node does not exist", "[graph][e
 	                    "Cannot call gdwg::graph<N, E>::erase_edge on src or dst if they don't exist in the graph");
 	REQUIRE_THROWS_WITH(g.erase_edge("B", "A"),
 	                    "Cannot call gdwg::graph<N, E>::erase_edge on src or dst if they don't exist in the graph");
+}
+
+TEST_CASE("Erase an edge using iterator") {
+	auto g = gdwg::graph<std::string, int>{"A", "B", "C"};
+
+	g.insert_edge("A", "B", 1);
+	g.insert_edge("A", "C", 2);
+	g.insert_edge("B", "C", 3);
+
+	auto it = g.find("A", "B", 1);
+	auto next_it = g.erase_edge(it);
+
+	REQUIRE((*next_it).from == "A");
+	REQUIRE((*next_it).to == "C");
+	REQUIRE((*next_it).weight == 2);
+
+	REQUIRE(g.is_connected("A", "B") == false);
+}
+
+TEST_CASE("Erase the last edge using iterator") {
+	auto g = gdwg::graph<std::string, int>{"A", "B"};
+
+	g.insert_edge("A", "B", 1);
+
+	auto it = g.find("A", "B", 1);
+	auto next_it = g.erase_edge(it);
+
+	REQUIRE(next_it == g.end());
+
+	REQUIRE(g.is_connected("A", "B") == false);
 }
